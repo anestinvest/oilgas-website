59200c8126743f78630f1123c477a33f698ca573
anestinvest <640277a@gmail.com>
2026-01-30 19:52:03 +0200

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+// app/layout.tsx
+import "./globals.css";
+
+export const metadata = {
+  title: "OILGAS",
+  description: "Corporate website of OILGAS",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
